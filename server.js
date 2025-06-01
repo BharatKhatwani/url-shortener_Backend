740899ec1be6d724a3013d3e0a1e19a5138786ed
bharat <bharatkhatwani06@gmail.com>
2025-06-01 17:15:21 +0530

    'UPDATE THE LINK'

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const cors = require('cors');
 
 
 app.use(cors({
-  origin: 'https://url-shortener-fhgs.vercel.app',
+ origin: ['https://url-shortener-gcas.vercel.app', 'http://localhost:3000'],
+
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
